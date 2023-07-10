b9eaf690b380673423bf150e13097cff49db9f22
rahul2271 <rraahhuullch@gmail.com>
2023-07-10 10:57:21 +0530

    h

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleNavbarMenu() {
+    const navbarMenu = document.getElementById('navbarMenu');
+    const navbarToggle = document.querySelector('.navbar-toggle');
+  
+    navbarMenu.classList.toggle('active');
+    navbarToggle.classList.toggle('active');
+  }
+  
